c38df0e847486ef442b2e8c692f79ebff06640b7
Mickael <mickael.maciel@gmail.com>
2025-01-23 15:58:17 -0300

    EMBARCA-TECH

--- display_oled.c
+++ display_oled.c
@@ -10,6 +10,7 @@
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 
+//GITHUB - MICKAEL MACIEL // https://github.com/mickaelmaciell
 // Configurações de pinos
 const uint I2C_SDA = 14;
 const uint I2C_SCL = 15;
